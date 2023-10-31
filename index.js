edd44b0bbfc7f7d6c535059f4766a1ec2f03773b
R.H.Mohamed Riffath <107987180+riffathRHM@users.noreply.github.com>
2023-10-31 21:08:46 +0530

    create a test API route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then (()=>{
   console.log('connected to MongoDB');
@@ -12,3 +13,8 @@ const app = express();
 app.listen(3000,()=>{
    console.log('server is running on port 3000');
 });
+
+// create a API Route
+//send object using json
+//we export router from user.route.js and we imoprt name as userRouter
+app.use("/api/user",userRouter);
