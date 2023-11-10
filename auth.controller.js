76e9e4db2b603a099f7fd1e5cb4dc9f118a61100
R.H.Mohamed Riffath <107987180+riffathRHM@users.noreply.github.com>
2023-11-11 00:03:31 +0530

    create sign up api route

---
+++ auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+//encrypt the password from bcryptjs package
+import bcryptjs from 'bcryptjs';
+
+
+
+
+
+// asynchrones for await because of the internet speed
+export const signup = async(req,res)=>{
+  // database conectivity
+  const{username,email,password} = req.body;
+  //password encrypt
+  const hashedPassword = bcryptjs.hashSync(password,10);
+  const  newUser = new User({username,email,password:hashedPassword});
+  // save inside the database
+  // when use same name and email it will get an error in saver now error show in fromtend
+  try{
+    await newUser.save();
+    res.status(201).json("user created sucessfully");
+  }
+  catch(error){
+    res.status(500).json(error.message);
+  }
+  
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import autheRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then (()=>{
   console.log('connected to MongoDB');
@@ -10,6 +11,10 @@ mongoose.connect(process.env.MONGO).then (()=>{
 })
 // we have to encrypt that link so in .env file
 const app = express();
+
+//allow to use JSON as a input from the saver
+app.use(express.json());
+
 app.listen(3000,()=>{
    console.log('server is running on port 3000');
 });
@@ -18,3 +23,4 @@ app.listen(3000,()=>{
 //send object using json
 //we export router from user.route.js and we imoprt name as userRouter
 app.use("/api/user",userRouter);
+app.use("/api/auth",autheRouter);
